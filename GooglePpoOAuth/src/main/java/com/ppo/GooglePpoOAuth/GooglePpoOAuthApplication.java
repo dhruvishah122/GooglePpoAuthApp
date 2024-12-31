e9a3a4dd89ecbad061e88d7ed961badb2b705bc0
dhruvishah122 <patvaupasana@gmail.com>
2024-12-31 15:58:16 +0530

    OAuth2 Client (Login with Google and Github) Spring Security

---
+++ GooglePpoOAuth/src/main/java/com/ppo/GooglePpoOAuth/GooglePpoOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ppo.GooglePpoOAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglePpoOAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglePpoOAuthApplication.class, args);
+	}
+
+}
---
+++ GooglePpoOAuth/src/main/java/com/ppo/GooglePpoOAuth/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.ppo.GooglePpoOAuth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String greet(){
+        return "hey bro:)";
+    }
+}
